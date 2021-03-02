6ee36435595ed647acd60f0e324cc44efec53899
Misha <mishazaika789@gmail.com>
2021-03-03 01:07:45 +0200

    Add project files.

---
+++ OOKP_Lab1.1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace OOKP_Lab1._1
+{
+    class Person
+    {
+        public string name; 
+        public int age;     
+
+        public void GetInfo()
+        {
+            Console.WriteLine($"Имя: {name}  Возраст: {age}");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person Misha = new Person();
+            Misha.name = "Misha";
+            Misha.age = 18;
+            Misha.GetInfo();
+            
+        }
+    }
+}
+
