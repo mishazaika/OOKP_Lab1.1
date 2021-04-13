d2897880ae1b3864f3adc80d2d2e3f54cdbb8e6e
Misha <mishazaika789@gmail.com>
2021-04-13 23:41:03 +0300

    Якість коду

--- OOKP_Lab1.1/Program.cs
+++ OOKP_Lab1.1/Program.cs
@@ -6,27 +6,32 @@ namespace OOKP_Lab1._1
     {
         public string name; 
         public int age;
-
+        public string hobby;
+        public Person(string n, int a)
+        {
+            name = n;
+            age = a;
+            GetInfo();
+            Hobby();
+        }
         public void GetInfo()
         {
-            Console.WriteLine($"Имя: {name}  Возраст: {age}");
+            Console.WriteLine($"Name: {name}  Age: {age}");
         }
-
+        public void Hobby()
+        {
+            Console.WriteLine($"Whats your hobby?");
+            hobby = Console.ReadLine();
+            Console.WriteLine($"Your hobby is {hobby}");
+        }
     }
     class Program
     {
         static void Main(string[] args)
         {
-            Person Misha = new Person();
-            Misha.name = "Misha!!!Conflict!!!";
-            Misha.age = 18;
-            Misha.GetInfo();
+            Person Misha = new Person("Misha", 18);
 
-            Person Sam = new Person();
-            Sam.name = "Sam";
-            Sam.age = 25;
-            Sam.GetInfo();
-            
+            Person Sam = new Person("Sam", 25);
         }
     }
 }
